6a2e425b7757c096839dda741415605ba3719c55
9444302 <90798635+9444302@users.noreply.github.com>
2021-10-21 11:04:55 -0600

    mobile friendly

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <div className="source-code"><a
-        href="https://github.com/9444302/weather-react"
-        target="_blank"
-        rel="noreferrer"
-      >Open source code</a> by Tori</div>
+      <div className="container">
+        <Weather />
+        <div className="source-code">
+          <a
+            href="https://github.com/9444302/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          by Tori
+        </div>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -7,55 +7,55 @@ import "./weather.css"
 export default function Weather() {
     return (
       <div className="weather">
-        <h1>Weather App</h1>
         <form>
-          <input type="search" placeholder="Search" />
-          <input type="submit" value="Search" />
+          <div className="row">
+            <div className="col-9">
+              <input type="search" placeholder="Search" className="w-100" />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="w-100" />
+            </div>
+          </div>
         </form>
         <h2>New York</h2>
         <h3>Friday 06:45</h3>
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
+        <h3>Sunny</h3>
+        <div className="row main">
+          <div className="col-sm-6">
               <ReactAnimatedWeather
                 icon={"CLEAR_DAY"}
                 color={"black"}
                 size={50}
                 animate={true}
               />
-              <span className="deg"> 17°C</span>
-              <br />
-              <div className="des">Sunny</div>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Humidity 30%</li>
-                <li>Precipitation</li>
-                <li>Wind 4 km/hr</li>
-              </ul>
-            </div>
+            <br className="d-block d-sm-none" />
+            <span className="deg"> 17°C</span>
+          </div>
+          <div className="col-sm-6">
+            <ul>
+              <li>Humidity 30%</li>
+              <li>Wind 4 km/hr</li>
+            </ul>
           </div>
         </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-2">
-              <ForcastDay day="Fri" icon="CLEAR_DAY" temp="17" />
-            </div>
-            <div className="col-2">
-              <ForcastDay day="Sat" icon="CLOUDY" temp="17" />
-            </div>
-            <div className="col-2">
-              <ForcastDay day="Sun" icon="RAIN" temp="19" />
-            </div>
-            <div className="col-2">
-              <ForcastDay day="Mon" icon="CLEAR_DAY" temp="18" />
-            </div>
-            <div className="col-2">
-              <ForcastDay day="Tues" icon="CLOUDY" temp="15" />
-            </div>
-            <div className="col-2">
-              <ForcastDay day="Wed" icon="RAIN" temp="20" />
-            </div>
+        <div className="row">
+          <div className="col-6 col-sm-2">
+            <ForcastDay day="Fri" icon="CLEAR_DAY" temp="17" />
+          </div>
+          <div className="col-6 col-sm-2">
+            <ForcastDay day="Sat" icon="CLOUDY" temp="17" />
+          </div>
+          <div className="col-6 col-sm-2">
+            <ForcastDay day="Sun" icon="RAIN" temp="19" />
+          </div>
+          <div className="col-6 col-sm-2">
+            <ForcastDay day="Mon" icon="CLEAR_DAY" temp="18" />
+          </div>
+          <div className="col-6 col-sm-2">
+            <ForcastDay day="Tues" icon="CLOUDY" temp="15" />
+          </div>
+          <div className="col-6 col-sm-2">
+            <ForcastDay day="Wed" icon="RAIN" temp="20" />
           </div>
         </div>
       </div>
