a662ddf87727de0a153111423323140621a8cd1c
9444302 <90798635+9444302@users.noreply.github.com>
2021-10-22 11:17:26 -0600

    added weather info

--- src/Weather.js
+++ src/Weather.js
@@ -1,64 +1,75 @@
-import React from "react"
-import ReactAnimatedWeather from "react-animated-weather";
+import React, { useState } from "react"
 import ForcastDay from "./ForcstDay";
 import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 
 import "./weather.css"
 
 export default function Weather() {
+  const [weather, setWeather] = useState({read: false})
+  const [city, setCity] = useState("Paris")
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=37bec3b1fe145a2ee30d91fc33082fd5&units=imperial`;
+
+  function getWeather(response){
+    setWeather({
+      ready: true,
+cityName: response.data.name
+    })
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+  }
+  function getCity(){
+ axios.get(apiUrl).then(getWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+if (weather.ready){
   return (
-      <div className="weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input type="search" placeholder="Search" className="w-100" />
-            </div>
-            <div className="col-3">
-              <input type="submit" value="Search" className="w-100" />
-            </div>
+    <div className="weather">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search"
+              className="w-100"
+              onChange={updateCity}
+            />
           </div>
-        </form>
-        <h2>New York</h2>
-        <h3>Friday 06:45</h3>
-        <h3>Sunny</h3>
-        <div className="row main">
-          <div className="col-sm-6">
-              <ReactAnimatedWeather
-                icon={"CLEAR_DAY"}
-                color={"black"}
-                size={50}
-                animate={true}
-              />
-            <br className="d-block d-sm-none" />
-            <span className="deg"> 17°C</span>
-          </div>
-          <div className="col-sm-6">
-            <ul>
-              <li>Humidity 30%</li>
-              <li>Wind 4 km/hr</li>
-            </ul>
+          <div className="col-3">
+            <input type="submit" value="Search" className="w-100" />
           </div>
         </div>
-        <div className="row">
-          <div className="col-6 col-sm-2">
-            <ForcastDay day="Fri" icon="CLEAR_DAY" temp="17" />
-          </div>
-          <div className="col-6 col-sm-2">
-            <ForcastDay day="Sat" icon="CLOUDY" temp="17" />
-          </div>
-          <div className="col-6 col-sm-2">
-            <ForcastDay day="Sun" icon="RAIN" temp="19" />
-          </div>
-          <div className="col-6 col-sm-2">
-            <ForcastDay day="Mon" icon="CLEAR_DAY" temp="18" />
-          </div>
-          <div className="col-6 col-sm-2">
-            <ForcastDay day="Tues" icon="CLOUDY" temp="15" />
-          </div>
-          <div className="col-6 col-sm-2">
-            <ForcastDay day="Wed" icon="RAIN" temp="20" />
-          </div>
+      </form>
+      <WeatherInfo data={weather}/>
+      <div className="row">
+        <div className="col-6 col-sm-2">
+          <ForcastDay day="Fri" icon="CLEAR_DAY" temp="17" />
+        </div>
+        <div className="col-6 col-sm-2">
+          <ForcastDay day="Sat" icon="CLOUDY" temp="17" />
+        </div>
+        <div className="col-6 col-sm-2">
+          <ForcastDay day="Sun" icon="RAIN" temp="19" />
+        </div>
+        <div className="col-6 col-sm-2">
+          <ForcastDay day="Mon" icon="CLEAR_DAY" temp="18" />
+        </div>
+        <div className="col-6 col-sm-2">
+          <ForcastDay day="Tues" icon="CLOUDY" temp="15" />
+        </div>
+        <div className="col-6 col-sm-2">
+          <ForcastDay day="Wed" icon="RAIN" temp="20" />
         </div>
       </div>
-    );
-}
+    </div>
+  );
+} else {
+getCity();
+  return (
+    "loading"
+  )}}
